7d7497273a7894461ff5713a58d3e7f409f2c893
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2021-01-21 15:07:59 -0500

    created the pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <NavBar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
   );
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../App.js';
+
+export default function SignUp() {
+  
+    return <h1 className="sign-up">Sign Up</h1>;
+ 
+}
